080ec616284dfa35872f90411fcc2c5175a2effa
sislight0224 <sislight0224@gmail.com>
2024-06-01 00:38:11 +1000

    move bomb detail script

---
+++ Assets/Scripts/UI/Game/MoveItemDetailPanel.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoveItemDetailPanel : MonoBehaviour
+{
+    public ItemCard itemPrefab;
+
+    public GameObject itemListPanel;
+
+    public Text posText;
+
+    public void Init() {
+
+    }
+}
